3403f66340110ef8415ba110d3068446d5714482
Prado Arturo Bognot <prado.bognot@gmail.com>
2018-08-09 12:00:39 +0800

    - Barangays under the municipality

--- application/controllers/Municipality.php
+++ application/controllers/Municipality.php
@@ -25,6 +25,19 @@ class Municipality extends CI_Controller {
     }
   }
 
+  // API for getting all barangays under the municipality
+  public function all_barangays($municipality_id=1) {
+    $data['all_barangays'] = $this->municipality_model->get_barangays_of_municipality($municipality_id);
+
+    if (empty($data['all_barangays'])) {
+      show_404();
+      return;
+    }
+    else {
+      echo json_encode($data['all_barangays']);  
+    }
+  }
+
   // API for weather
   public function weather($municipality_id=1, $date_end=null, $date_start=null) {
     $data['weather'] = $this->municipality_model->get_weather($municipality_id);
--- application/models/Municipality_model.php
+++ application/models/Municipality_model.php
@@ -17,17 +17,21 @@ class Municipality_model extends CI_Model {
     // TODO: query composition based on date variables
 
     $query = $this->db->query($query_text);
-    return $query->row_array();
+    return $query->result_array();
   }
 
   // TODO: Get all barangays under the municipality
   public function get_barangays_of_municipality($id=1) {
-    
+    $query_text = 'SELECT name, population_vulnerable FROM barangay_basic_info';
+    // $query_text += ' WHERE municipality_id=' . $id;
+    // echo $query_text;
+
+    $query = $this->db->query($query_text);
+    return $query->result_array();
   }
 
   // TODO: Get alert levels of the municipality
   public function get_alert_levels($id=1, $date_end=null, $date_start=null) {
-    
   }
 
 }
