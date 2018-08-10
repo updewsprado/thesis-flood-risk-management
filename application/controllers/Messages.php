66cbe6fc4a8ac388621ff638f41a12c744426dc8
Prado Arturo Bognot <prado.bognot@gmail.com>
2018-08-10 09:15:15 +0800

    - Messages API documentation

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -7,11 +7,26 @@ class Messages extends CI_Controller {
     $this->load->helper('url_helper');
   }
 
+  // ex URL: http://localhost/messages
+  /*
+    ex output:
+
+    Messages Index Page
+  */
   public function index() {
     $data['title'] = 'Messages Index Page';
     $this->load->view('messages/index', $data);
   }
 
+  // ex URL: http://localhost/messages/facebook/1
+  /*
+    ex output:
+
+    [{"id":"1","municipality_id":"1","ts":"2018-06-12 17:00:00",
+    "sender":"MERALCO","message":"There will be power interruption in Tabing-ilog, Loma De Gato and Ibayo"},
+    {"id":"2","municipality_id":"1","ts":"2018-06-12 17:00:00",
+    "sender":"NAWASA","message":"Expect water supply to be muddy for the whole Marilao"}]
+  */
   public function facebook($municipality_id=1) {
     $data['facebook'] = $this->messages_model->get_facebook_messages($municipality_id);
 
@@ -23,6 +38,15 @@ class Messages extends CI_Controller {
     }
   }
 
+  // ex URL: http://localhost/messages/twitter/1
+  /*
+    ex output:
+
+    [{"id":"1","municipality_id":"1","ts":"2018-07-12 19:35:00",
+    "sender":"concerned citizen","message":"Wala pong kuryente dito sa may Abangan "},
+    {"id":"2","municipality_id":"1","ts":"2018-07-12 19:35:00",
+    "sender":"tambay","message":"Stranded na po kami dito sa may SM Marilao"}]
+  */
   public function twitter($municipality_id=1) {
     $data['twitter'] = $this->messages_model->get_twitter_messages($municipality_id);
 
