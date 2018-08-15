8245ec341c8057e3babfa4a69ae7d7654d673819
Prado Arturo Bognot <prado.bognot@gmail.com>
2018-08-15 21:09:38 +0800

    - Readying template functions

--- static/app/app.frms.js
+++ static/app/app.frms.js
@@ -1,13 +1,54 @@
 (function() {
   'use strict';
 
-  var app = angular.module('frmsApp', []);
+  var app = angular.module('frmsApp', ['ui.router']);
 
   app.controller('mainCtrl', function($log, $scope, $http, $window) {
 
     $log.debug("mainController has been initiated!");
     var vm = $scope;
-    vm.municipality = "Marilao, Bulacan";
+
+    // Function Declarations
+    vm.getActionBoardMunicipality = getActionBoardMunicipality;
+    vm.getCurrentDate = getCurrentDate;
+    vm.getMarkerLevels = getMarkerLevels;
+    vm.getMessagesFacebook = getMessagesFacebook;
+    vm.getMessagesTwitter = getMessagesTwitter;
+
+    // Variables
+    vm.municipality = {
+      name: "Marilao, Bulacan",
+    };
+
+    // Function Definitions
+
+    // TODO: Get the Municipality Action Board
+    function getActionBoardMunicipality(targetDate) {
+      $log.debug("getActionBoardMunicipality function: target date = ", targetDate);
+    }
+
+    // TODO: Get current date (to be used as input for other functions)
+    function getCurrentDate() {
+      $log.debug("getCurrentDate function");
+    }
+
+    // TODO: Get current Marker Levels
+    function getMarkerLevels(targetDate) {
+      $log.debug("getMarkerLevels function: target date = ", targetDate);
+    }
+
+    // TODO: Collate facebook messages
+    function getMessagesFacebook(targetDate) {
+      $log.debug("getMessagesFacebook function: target date = ", targetDate);
+    }
+
+    // TODO: Collate twitter messages
+    function getMessagesTwitter(targetDate) {
+      $log.debug("getMessagesTwitter function: target date = ", targetDate);
+    }
+
+
+    
   });
 
 })();
