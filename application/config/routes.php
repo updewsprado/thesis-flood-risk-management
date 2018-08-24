8d9d8453094645aad0466d5f8f5b6ab2862b9a5a
Prado Bognot <updews.prado@gmail.com>
2018-08-25 01:54:55 +0800

    - fixed routing

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = 'main';
+$route['default_controller'] = 'main/angular_main';
 $route['data_admin'] = 'main/data_admin';
 
 // $route['default_controller'] = 'pages/view';
